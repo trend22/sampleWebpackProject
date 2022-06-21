0f18325926e139e9e1309aa8b1b20440dc31eaba
Nick Aleksapolskiy <trend22@mail.ru>
2022-06-21 23:13:08 +0300

    lesson19Date started

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,76 @@
-(()=>{var o={609:o=>{o.exports=()=>{console.log("One function started!")}},219:o=>{o.exports=()=>{console.log("Two function started!")}}},t={};function r(e){var n=t[e];if(void 0!==n)return n.exports;var s=t[e]={exports:{}};return o[e](s,s.exports,r),s.exports}(()=>{const o=r(609),t=r(219);o(),t()})()})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("const one = __webpack_require__(/*! ./modules/one */ \"./modules/one.js\")\r\nconst two = __webpack_require__(/*! ./modules/two */ \"./modules/two.js\")\r\n\r\none()\r\ntwo()\n\n//# sourceURL=webpack:///./index.js?");
+
+/***/ }),
+
+/***/ "./modules/one.js":
+/*!************************!*\
+  !*** ./modules/one.js ***!
+  \************************/
+/***/ ((module) => {
+
+eval("const one = () => {\r\n    console.log('One function started!')\r\n}\r\n\r\nmodule.exports = one\n\n//# sourceURL=webpack:///./modules/one.js?");
+
+/***/ }),
+
+/***/ "./modules/two.js":
+/*!************************!*\
+  !*** ./modules/two.js ***!
+  \************************/
+/***/ ((module) => {
+
+eval("const two = () => {\r\n    console.log('Two function started!')\r\n}\r\n\r\nmodule.exports = two\n\n//# sourceURL=webpack:///./modules/two.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./index.js");
+/******/ 	
+/******/ })()
+;
